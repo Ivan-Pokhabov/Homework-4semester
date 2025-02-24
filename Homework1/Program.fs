343586fe31873209397cdb7c4e1b6b6c341ada52
Ivan Pokhabov <vanek3372@gmail.com>
2025-02-24 21:18:11 +0300

    fix: some changes after review

--- Homework1/Program.fs
+++ Homework1/Program.fs
@@ -14,22 +14,23 @@ let factorial n =
 
 /// Function to compute fibonacci number by its position
 let fibonacci n =
-        if n < 0 then
-            None
-        else
-            let rec recFibonacci n cur next =
-                match n with
-                | 0 -> Some(cur)
-                | _ -> recFibonacci (n - 1) next (cur + next)
+        if n < 0 then None else
         
-            recFibonacci n 0 1
+        let rec recFibonacci n cur next =
+            match n with
+            | 0 -> Some(cur)
+            | _ -> recFibonacci (n - 1) next (cur + next)
+        
+        recFibonacci n 0 1
 
 
 /// Function to reverse a list
-let rec reverse list =
-    match list with
-    | [] -> []
-    | head :: tail -> (reverse tail) @ [head] 
+let reverse list =
+    let rec reverseAcc acc lst =
+        match lst with
+        | [] -> acc
+        | head :: tail -> reverseAcc (head :: acc) tail
+    reverseAcc [] list
 
 
 /// Function to find the first occurrence of a given element in a list
@@ -43,9 +44,22 @@ let findFirst list x =
 
     recFindFirst list 0
 
+/// Function for binary exponentiation
+let rec binpow num exp =
+    match exp with
+    | 0 -> 1
+    | _ when exp % 2 = 0 -> let half = binpow num (exp / 2) in half * half
+    | _ -> num * binpow num (exp - 1)
+
 
 /// Function to find list of degrees from 2^n to 2^(n+m)
 let powerList n m =
-    List.scan (fun acc _ -> acc * 2) (pown 2 n) [1..m]
+    let rec loop acc count currentValue =
+        if count > m then
+            acc
+        else
+            loop (currentValue :: acc) (count + 1) (currentValue * 2)
+    let initialValue = binpow 2 n
+    loop [] 1 initialValue |> reverse
 
 
