5d61cc5340577eb68bd90384d78aed7121f2aa4a
Ivan Pokhabov <vanek3372@gmail.com>
2025-03-10 22:32:05 +0300

    file: switch position for testing and adding fsproj

---
+++ homework2/Homework2/EvenNumbers.fs
@@ -0,0 +1,10 @@
+﻿module Homework2.EvenNumbers
+
+let countEvenNumbersByMap list =
+    list |> List.map (fun number -> if number % 2 = 0 then 1 else 0) |> List.sum
+
+let countEvenNumbersByFilter list =
+    list |> List.filter (fun number -> if number % 2 = 0 then true else false) |> List.length
+
+let countEvenNumbersByFold list =
+    list |> List.fold (fun acc number -> if number % 2 = 0 then acc + 1 else acc) 0
