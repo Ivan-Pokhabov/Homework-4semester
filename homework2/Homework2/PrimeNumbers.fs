4f71f878bbad40d920530f8992f4b7051c6118cb
Ivan Pokhabov <vanek3372@gmail.com>
2025-03-10 22:29:49 +0300

    feat: add implementation of prime numbers task

---
+++ homework2/Homework2/PrimeNumbers.fs
@@ -0,0 +1,12 @@
+module Homework2.PrimeNumbers
+
+let isPrime number = 
+    if number < 2 then
+        false
+    else
+        let root = float number |> sqrt |> int
+        seq { 2 .. root } |> Seq.forall (fun i -> number % i <> 0)
+
+let getPrime() =
+    Seq.initInfinite ((+) 2)
+    |> Seq.filter isPrime
