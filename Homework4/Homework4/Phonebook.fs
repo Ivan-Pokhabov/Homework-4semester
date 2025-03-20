e973b4bc5f35c4add8ded7a9ebf42b3e975a4d0e
Ivan Pokhabov <vanek3372@gmail.com>
2025-03-20 23:59:07 +0300

    feat: solve task phonebook

--- Homework4/Homework4/Phonebook.fs
+++ Homework4/Homework4/Phonebook.fs
@@ -28,12 +28,12 @@ let readFromFile path =
         System.IO.File.ReadAllLines path
         |> Array.toList
         |> List.map (fun line -> 
-            let parts = line.Split('-')
+            let parts = line.Split(':')
             Contact(parts.[0], parts.[1])
             )
         |> Phonebook
         |> Some
 
 let storeToFile(path, phonebook) =
-    let lines = List.map (fun (name, phone) -> sprintf "%s-%s" name phone) (getPhonebookData phonebook)
+    let lines = List.map (fun (name, phone) -> sprintf "%s:%s" name phone) (getPhonebookData phonebook)
     System.IO.File.WriteAllLines(path, List.toArray lines)
