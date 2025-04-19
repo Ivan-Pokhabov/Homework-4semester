4737b5f0428841e3626219ec3aa76c001d293841
Ivan Pokhabov <vanek3372@gmail.com>
2025-04-19 22:39:36 +0300

    feat&test: solve task lazy

---
+++ Homework7/Lazy/Program.fs
@@ -0,0 +1,49 @@
+﻿module Lazy
+open System.Threading
+
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
+
+type LazySimple<'a>(supplier: unit -> 'a) =
+        let mutable result = None
+        interface ILazy<'a> with
+            member _.Get() =
+                match result with
+                | Some value -> value
+                | None ->
+                    let computed = supplier()
+                    result <- Some computed
+                    computed
+
+type LazyConcurrent<'a>(supplier: unit -> 'a) =
+    let mutable result = None
+    let lockObj = obj()
+    interface ILazy<'a> with
+        member _.Get() =
+            match result with
+            | Some value -> value
+            | None ->
+                lock lockObj (fun () ->
+                    match result with
+                    | Some value -> value
+                    | None ->
+                        let computed = supplier()
+                        result <- Some computed
+                        computed
+                )
+
+type LazyLockFree<'a>(supplier: unit -> 'a) =
+    let mutable result = null
+    interface ILazy<'a> with
+        member _.Get() =
+            let current = result
+            if current <> null then
+                unbox current
+            else
+                let computed = supplier()
+                let boxed = box computed
+                let original = Interlocked.CompareExchange(&result, boxed, null)
+                if original = null then
+                    computed
+                else
+                    unbox original
